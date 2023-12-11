3f6a1ef8d1306e6255b43d71ab08d642d23eb6f9
m.kotoma <makoto.866281@outlook.com>
2023-12-11 13:58:43 +0000

    add bootstrap design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   
   def index
-    @tasks = Task.all
+    #@tasks = Task.all
+    @pagy, @tasks = pagy(Task.all.order(id: :asc), items: 3)
   end
   
   def show
